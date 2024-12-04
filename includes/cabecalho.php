2854c3c4446964d3557376de54c9776b7b166aa9
ruandelia <ruandelia2@gmail.com>
2024-12-04 14:57:47 -0300

    Update cabecalho.php

---
+++ includes/cabecalho.php
@@ -0,0 +1,27 @@
+<!-- cabecalho.php -->
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DICA§AÚDE</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="estilos.css" rel="stylesheet">
+</head>
+<body>
+
+    <!-- Barra de navegação -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <a class="navbar-brand text-white" href="#">DICA§AÚDE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a class="nav-link text-white" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link text-white" href="#">Sobre</a></li>
+                <li class="nav-item"><a class="nav-link text-white" href="#">Contato</a></li>
+            </ul>
+        </div>
+    </nav>
